3007b6d21e7017d9eef14fad08be01762fb0695a
douqin <dagxuanlam@gmail.com>
2025-03-09 15:19:36 +0700

    update: responseData for res

--- src/utils/response.ts
+++ src/utils/response.ts
@@ -1,13 +1,32 @@
 export default class ResponseData<T> {
-  status: number;
-  message: string[];
-  error: string[];
-  data: T | null;
+  status: number
+  message: string[]
+  error: string[]
+  data: T | null
 
-  constructor(status: number, message : string[] = [], error : string[] = [], data : T | null = null) {
-    this.status = status;
-    this.message = message;
-    this.error = error;
-    this.data = data;
+  constructor(status: number, message: string[] = [], error: string[] = [], data: T | null = null) {
+    this.status = status
+    this.message = message
+    this.error = error
+    this.data = data
+  }
+
+  static success<T>(data: T): ResponseData<T> {
+    return new ResponseData(200, [], [], data)
+  }
+  static failure<T>(status: number, message: string, error: string) {
+    return new ResponseData(status, [message], [error], null)
+  }
+  static error<T>(status: number, message: string, error: string) {
+    return new ResponseData(status, [message], [error], null)
+  }
+  static notFound<T>(message: string) {
+    return new ResponseData(404, [message], [], null)
+  }
+  static unauthorized<T>(message: string) {
+    return new ResponseData(401, [message], [], null)
+  }
+  static badRequest<T>(message: string) {
+    return new ResponseData(400, [message], [], null)
   }
 }
