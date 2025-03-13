f773fd77f589c4be686a0b09246d8add66456536
tungpy78 <truonganhtung230904@gmail.com>
2025-03-13 17:11:30 +0700

    Chua fix token

--- src/web/routers/auth.router.ts
+++ src/web/routers/auth.router.ts
@@ -6,4 +6,11 @@ const authRouter = express.Router()
 // Use controller instance methods (with proper binding)
 authRouter.post('/forgot-password', authController.forgotPassword.bind(authController))
 
+//verify-otp
+authRouter.post('/verify-otp', authController.verifyOtp.bind(authController))
+
+//reset-password
+authRouter.post('/reset-password', authController.resetPassword.bind(authController))
+
+
 export default authRouter
