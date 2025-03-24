30bd3eb48312847e30d2952249d9a29b9f0fd64b
douqin <dagxuanlam@gmail.com>
2025-03-24 16:41:53 +0700

    feature: add router and controller for admin

--- src/web/routers/router-config.ts
+++ src/web/routers/router-config.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import exampleRouter from './example.router'
 import authRouter from './auth.router'
 import multiMediaRouter from './multimedia.router'
+import adminRouter from './admin.router'
 
 const routerConfig = express.Router()
 
@@ -11,5 +12,7 @@ routerConfig.use('/files', multiMediaRouter)
 routerConfig.use(`/examples`, exampleRouter)
 
 routerConfig.use('/auth', authRouter)
+
+routerConfig.use('/admin', adminRouter)
 
 export default routerConfig
