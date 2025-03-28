c0c8b05fda026c127fce564d0bf7678a7fbc040d
douqin <dagxuanlam@gmail.com>
2025-03-28 10:18:10 +0700

    fix: otp

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -2,17 +2,22 @@ import redis from '@/infras/redis/redis'
 import { generateRandomNumber } from '@/utils/config/generate.helper'
 import bcrypt from 'bcryptjs'
 import { Result } from '@/utils/data-types/result'
-import { promisify } from 'util'
 import { AccountRepository, CustomerRepository, AdminRepository } from '@/infras/repositories'
 import JWTService from './jwt.service'
 import { MailService } from './mail.service'
-import { text } from 'stream/consumers'
-const ttlAsync = promisify(redis.ttl).bind(redis) // Chuyển ttl thành Promise
 
 export default class AuthService {
   private accountRepository: AccountRepository
+  private customerRepository: CustomerRepository
   private mailService: MailService
   private jwtService: JWTService
+
+  constructor() {
+    this.accountRepository = new AccountRepository()
+    this.mailService = new MailService()
+    this.jwtService = new JWTService()
+    this.customerRepository = new CustomerRepository()
+  }
 
   async refreshToken(refreshToken: string) {
     const result = this.jwtService.verifyRefreshToken(refreshToken)
@@ -37,11 +42,6 @@ export default class AuthService {
     // }
     return Result.ok()
   }
-  constructor() {
-    this.accountRepository = new AccountRepository()
-    this.mailService = new MailService()
-    this.jwtService = new JWTService()
-  }
 
   async isEmail(email: string) {
     return await this.accountRepository.findOne({ where: { email: email } })
@@ -64,14 +64,14 @@ export default class AuthService {
     const key = `${keyRedis}:${email}`
 
     // Kiểm tra TTL của OTP hiện tại
-    const remainingTime = await ttlAsync(key)
+    const remainingTime = await redis.ttl(key)
 
     if (remainingTime && remainingTime > 0) {
       return Result.fail(429, `OTP đã được gửi. Vui lòng thử lại sau ${remainingTime} giây.`)
     }
     // Tạo và lưu OTP mới (hiệu lực 5 phút)
     const otp = generateRandomNumber(6)
-    await redis.set(`otp-forgot-password:${email}`, otp.toString(), 'EX', 300)
+    await redis.set(`${keyRedis}:${email}`, otp.toString(), 'EX', 300)
 
     // Gửi email
     await this.mailService.createTransporter()
@@ -85,12 +85,31 @@ export default class AuthService {
     return Result.ok({ message: 'OTP sent to email', remainingTime })
   }
 
+  async setVerifiedCustomer(email: string) {
+    const user = await this.isEmail(email)
+    if (!user) {
+      return Result.fail(404, 'EMAIL_NOT_FOUND')
+    }
+    const account = await this.accountRepository.findOne({ where: { email: email }, relations: ['customer'] })
+    if (!account) {
+      return Result.fail(404, 'ACCOUNT_NOT_FOUND')
+    }
+    const customer = await this.customerRepository.findOne({ where: { customerId: account.customer.customerId } })
+    if (!customer) {
+      return Result.fail(404, 'CUSTOMER_NOT_FOUND')
+    }
+    customer.isVerified = 1
+    await this.customerRepository.save(customer)
+    return Result.ok(true)
+  }
+
   /**
    * Xác thực OTP
    */
   async verifyOtp(keyRedis: string, email: string, otp: string) {
     // const storedOtp = await redis.get(`otp-forgot-password:${email}`)
     const storedOtp = await redis.get(`${keyRedis}:${email}`)
+    console.log(keyRedis, email, otp)
     if (!storedOtp || storedOtp !== otp) {
       return Result.fail(400, 'OTP_INVALID')
     }
