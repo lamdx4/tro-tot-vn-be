4f59a8f84ef45a2664549310fe4221b87fa2793c
douqin <dagxuanlam@gmail.com>
2025-04-02 23:26:55 +0700

    fix: add protect middleware for admin route

--- src/web/controllers/admin.controller.ts
+++ src/web/controllers/admin.controller.ts
@@ -1,49 +1,63 @@
-import { PostRepository } from '@/infras/repositories';
-import AdminService from '@/services/admin.service';
-import ResponseData from '@/utils/data-types/response';
+import { PostRepository } from '@/infras/repositories'
+import AdminService from '@/services/admin.service'
+import ResponseData from '@/utils/data-types/response'
 import { Request, Response, NextFunction } from 'express'
 
 class AdminController {
-    private adminService: AdminService;
+  private adminService: AdminService
 
-    constructor() {
-        const postRepository = new PostRepository(); // Khởi tạo repository
-        this.adminService = new AdminService(postRepository); // Truyền vào AdminService
-    }
+  constructor() {
+    this.adminService = new AdminService() 
+  }
 
-    
-    async reviewPost(req: Request, res: Response, next: NextFunction){
-        try {
-            const result = await this.adminService.ListPostPending()
-            if(result.isSuccess){
-                res.status(200).json(ResponseData.success(result.getValue()))
-            }else if(result.code === 404){
-                res.status(404).json(ResponseData.error(404, "No pending posts found", "No pending posts found"))
-            }else{
-                res.status(500).json(ResponseData.error(500, "Internal Server Error", "An unexpected error occurred")) 
-            }
-        } catch (error) {
-            console.error('Error in resetPassword:', error)
-            res.status(500).json(ResponseData.error(500, 'INTERNAL_ERROR', 'Something went wrong'))
-        }
+  async reviewPost(req: Request, res: Response, next: NextFunction) {
+    try {
+      const result = await this.adminService.listPostPending()
+      if (result.isSuccess) {
+        res.status(200).json(ResponseData.success(result.getValue()))
+      } else if (result.code === 404) {
+        res.status(404).json(ResponseData.error(404, 'No pending posts found', 'No pending posts found'))
+      } else {
+        res.status(500).json(ResponseData.error(500, 'Internal Server Error', 'An unexpected error occurred'))
+      }
+    } catch (error) {
+      console.error('Error in resetPassword:', error)
+      res.status(500).json(ResponseData.error(500, 'INTERNAL_ERROR', 'Something went wrong'))
     }
-    async changStatusPost(req: Request, res: Response, next: NextFunction){
-        try { 
-            const {status, message, postId} = req.body;
-            const result = await this.adminService.changStatusService(status, postId, message)
-            if(result.isSuccess){
-                res.status(200).json(ResponseData.success((await result).getValue()))
-            }else if(result.code === 400){
-                res.status(400).json(ResponseData.error(404, "Rejection reason is required when status is 'Reject'", "Rejection reason is required when status is 'Reject'"))
-            }else if(result.code === 404){
-                res.status(404).json(ResponseData.error(404, "Post not found or status not changed", "The specified post was not found or the status was not changed"))
-            }else{
-                res.status(500).json(ResponseData.error(500, "Internal Server Error", "An unexpected error occurred")) 
-            }
-        } catch (error) {
-            console.error('Error in resetPassword:', error)
-            res.status(500).json(ResponseData.error(500, 'INTERNAL_ERROR', 'Something went wrong'))
-        }
+  }
+  async moderatePost(req: Request, res: Response, next: NextFunction) {
+    try {
+      const { status, message, postId } = req.body
+      const result = await this.adminService.moderatePost(status, postId, message)
+      if (result.isSuccess) {
+        res.status(200).json(ResponseData.success(result.getValue()))
+      } else if (result.code === 400) {
+        res
+          .status(400)
+          .json(
+            ResponseData.error(
+              404,
+              "Rejection reason is required when status is 'Reject'",
+              "Rejection reason is required when status is 'Reject'"
+            )
+          )
+      } else if (result.code === 404) {
+        res
+          .status(404)
+          .json(
+            ResponseData.error(
+              404,
+              'Post not found or status not changed',
+              'The specified post was not found or the status was not changed'
+            )
+          )
+      } else {
+        res.status(500).json(ResponseData.error(500, 'Internal Server Error', 'An unexpected error occurred'))
+      }
+    } catch (error) {
+      console.error('Error in resetPassword:', error)
+      res.status(500).json(ResponseData.error(500, 'INTERNAL_ERROR', 'Something went wrong'))
     }
+  }
 }
 export default new AdminController()
