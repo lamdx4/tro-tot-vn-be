caa190d3040b246519624e9be61d65ae5ca4309d
douqin <dagxuanlam@gmail.com>
2025-04-08 15:30:24 +0700

    fix: remove appointment.repository.ts

--- src/infras/repositories/appointment.repository.ts
+++ src/infras/repositories/appointment.repository.ts
@@ -1,8 +1,8 @@
-import { Appointment } from '@/domains/entities/appointment.entity'
-import { BaseRepository } from './base.repository'
+// import { Appointment } from '@/domains/entities/appointment.entity'
+// import { BaseRepository } from './base.repository'
 
-export class AppointmentRepository extends BaseRepository<Appointment> {
-  constructor() {
-    super(Appointment)
-  }
-}
+// export class AppointmentRepository extends BaseRepository<Appointment> {
+//   constructor() {
+//     super(Appointment)
+//   }
+// }
