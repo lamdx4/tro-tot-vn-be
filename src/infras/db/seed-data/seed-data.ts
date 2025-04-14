aca8619364437627b45513e05086962f16b3a8f6
douqin <dagxuanlam@gmail.com>
2025-04-14 22:35:12 +0700

    fix: fixed manager account

--- src/infras/db/seed-data/seed-data.ts
+++ src/infras/db/seed-data/seed-data.ts
@@ -1,7 +1,9 @@
 import { RoleFactory } from './factories/role.factory'
 import { UserFactory } from './factories/user.factory'
+import { ManagerFactory } from './factories/admin.factory'
 
 export default async function seedData() {
   await RoleFactory.seedRolesAndPermissions()
   await UserFactory.seedUsers()
+  await ManagerFactory.seedManager()
 }
