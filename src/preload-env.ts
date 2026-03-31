e963b60b28792ea6edd2328f551997606c015450
lamdx4 <dagxuanlam@gmail.com>
2026-04-01 00:07:04 +0700

    fix: config ip server turn

--- src/preload-env.ts
+++ src/preload-env.ts
@@ -65,7 +65,7 @@ export const env = cleanEnv(process.env, {
 
   // WebRTC / Coturn Configuration
   STUN_SERVER_URLS: str(),
-  // TURN_SERVER_IP: str(),
+  TURN_SERVER_IP: str({ default: '' }), // Optional: if missing, will fallback to Redis discovery
   TURN_SERVER_PORT: port(),
   TURN_SERVER_TLS_PORT: port(),
   TURN_USERNAME: str(),
