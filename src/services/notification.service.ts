066c537b5fb8dda1413774ad5461c7e84859382f
lamdx4 <dagxuanlam@gmail.com>
2026-05-25 10:50:08 +0700

    fix: refactor to type-safe check by updating payload data type to Record<string, string | Date>

--- src/services/notification.service.ts
+++ src/services/notification.service.ts
@@ -42,7 +42,7 @@ export class NotificationService {
     userId: string | number,
     payload: {
       notification?: { title: string; body: string };
-      data?: Record<string, string>;
+      data?: Record<string, string | Date>;
       priority?: 'high' | 'normal';
       ttl?: number;
       androidTag?: string;
@@ -80,8 +80,8 @@ export class NotificationService {
     const stringData: Record<string, string> = {}
     if (payload.data) {
       Object.entries(payload.data).forEach(([key, value]) => {
-        if ((value as any) instanceof Date) {
-          stringData[key] = (value as any).toISOString()
+        if (value instanceof Date) {
+          stringData[key] = value.toISOString()
         } else {
           stringData[key] = String(value)
         }
